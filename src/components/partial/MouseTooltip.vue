348b5db2003f8b8819450fcf8d9c44f2b1db1e50
Andrey Nevinny <psoglav.ih8u@gmail.com>
2021-09-22 04:28:17 +0300

    burp

---
+++ src/components/partial/MouseTooltip.vue
@@ -0,0 +1,56 @@
+<template>
+  <div ref="tooltip" class="mouse-tooltip">{{ label }}</div>
+</template>
+
+<script>
+export default {
+  props: ['label'],
+  data() {
+    return {}
+  },
+  methods: {
+    init() {
+      const self = this
+      this.$el.querySelector('.mouse-tooltip')
+      document.addEventListener('mousemove', e => {
+        self.$refs.tooltip.style.top = e.clientY + 'px'
+        self.$refs.tooltip.style.left = e.clientX + 'px'
+      })
+      document.addEventListener('mousemove', e => {
+        self.$refs.tooltip.style.top = e.clientY + 'px'
+        self.$refs.tooltip.style.left = e.clientX + 'px'
+      })
+    },
+  },
+  mounted() {
+    this.init()
+  },
+}
+</script>
+
+<style lang="scss">
+@import '@/scss/_variables.scss';
+
+$radius: 17px;
+
+.mouse-tooltip {
+  position: fixed;
+  top: 0;
+  left: 0;
+  box-sizing: border-box;
+  margin: 15px;
+  margin-left: 30px;
+  z-index: 100;
+  background: #00000033;
+  backdrop-filter: blur(30px);
+  padding: 20px;
+  box-shadow: 0 10px 15px transparentize($light-blue, 0.9);
+  border: 1px solid $blue;
+  border-radius: 0px $radius $radius $radius;
+  user-select: none;
+  max-width: 18rem;
+  overflow-wrap: break-word;
+  text-align: left;
+  font-size: 14px;
+}
+</style>
--- src/components/views/profile/tabs/profile-tab/ProfileTab.vue
+++ src/components/views/profile/tabs/profile-tab/ProfileTab.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="profile-tab">
+    <!-- <mouse-tooltip label="Your account will be immediately deactivated and never be back." /> -->
     <identity-panel />
     <security-panel />
     <promo-input text="Promo code" icon="ticket-alt" />
@@ -14,6 +15,7 @@ import SecurityPanel from './panels/security-panel/SecurityPanel.vue'
 import PromoInput from './components/PromoInput.vue'
 import DeactivateAccount from './components/DeactivateAccount.vue'
 import Logout from './components/Logout.vue'
+// import MouseTooltip from '@/components/partial/MouseTooltip'
 
 export default {
   name: 'ProfileTab',
@@ -23,6 +25,7 @@ export default {
     PromoInput,
     DeactivateAccount,
     Logout,
+    // MouseTooltip,
   },
 }
 </script>
