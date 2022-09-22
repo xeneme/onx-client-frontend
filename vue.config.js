1dd47985a0f45b7637aa9af3845241ac90439870
Andrey Nevinny <psoglav.ih8u@gmail.com>
2022-09-22 23:40:54 +0300

    chore: import global.scss in vue config

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,11 @@ module.exports = {
   chainWebpack: config => {
     config.plugins.delete('prefetch')
   },
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: '@import "@/scss/global";'
+      }
+    }
+  }
 }
